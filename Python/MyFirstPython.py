9c26be7c4b4929e4d89f9594a6df97ee864fe7d1
security-cap <roypulak@protonmail.com>
2024-06-03 06:27:59 -0400

    Myfirstpython.py commit

---
+++ Python/MyFirstPython.py
@@ -0,0 +1,13 @@
+ #Hello this is my first Python program.
+ #This program says Hello and asked for your name
+hello = 'Hello, world'
+print(hello)
+print() #to put a blunk line
+print('What is your name?') #ask for their name
+myName = input()
+print('It is good to meet you' + myName)
+print('The length fo your name is:')
+print(len(myName))
+print('What is your age')
+myAge =input()
+print('You will be ' + str(int(myAge)+1) + ' in a year.')
