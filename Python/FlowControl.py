4700bddfad84d3e087b70e9a16b6063b9dc6d835
security-cap <roypulak@protonmail.com>
2024-06-07 12:28:59 -0400

    this commit for flow control

---
+++ Python/FlowControl.py
@@ -0,0 +1,61 @@
+###### flow control with if else condition.
+
+
+# print('Enter your name')
+# n = input()
+# print()
+# print('Enter your pass')
+# p = input()
+# print()
+
+# name = 'pulak'
+# passwd = 'asdf'
+# if name == n:
+#         print('hello pulak')
+#         if passwd == p:
+#                 print('acces done')
+#         else:
+#                 print('acces denait')
+# else:
+#         print('name not found')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###### flow control with elif(elseif) condition.
+
+# print('Enter your name')
+# name = input()
+# print('Enter your age')
+# age = int(input())
+# if name == 'pulak':
+#         print('Hi! pulak')
+# elif 12 > age :
+#         print('you are not pulak')
+# else :
+#         print('pulak enter your correct name')
+
+###############another program of else if
+# name = 'pulak'
+# age = 13
+
+# if name == 'pulak':
+#         print('Hi! pulak')
+# elif age < 15 :
+#         print('you are not pulak! you are a kid.')
+# elif age > 1200:
+#         print('your are a vampair')
+# elif age > 30:
+#         print('ha ha ha yes')
